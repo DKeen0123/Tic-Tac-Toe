b333a6243b5d2c73d7f623f1b31cf15808d57944
Daniel Keen <keendaniel01@gmail.com>
2018-03-29 15:41:48 +0100

    feat: Wrapper renders a PlayerTurn component

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import PlayerTurn from './PlayerTurn';
 
 class Wrapper extends Component {
   render() {
     return (
       <div>
         <Header />
+        <PlayerTurn />
       </div>
     );
   }
