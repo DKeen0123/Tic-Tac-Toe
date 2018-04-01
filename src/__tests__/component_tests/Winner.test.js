e15eac4ec15b6e9e91d3ea6a223a64839de543a5
Daniel Keen <keendaniel01@gmail.com>
2018-04-01 19:15:26 +0100

    test: Winner component renders correct winner :white_check_mark:

--- src/__tests__/component_tests/Winner.test.js
+++ src/__tests__/component_tests/Winner.test.js
@@ -3,8 +3,24 @@ import { shallow } from 'enzyme';
 import Winner from '../../components/Winner';
 
 describe('Winner', () => {
-  const winner = shallow(<Winner />);
+  let props = { winningPlayer: 'Player 1' };
+  let winner = shallow(<Winner {...props} />);
+
   it('renders correctly', () => {
     expect(winner).toMatchSnapshot();
+  });
+
+  describe('when player 1 wins', () => {
+    it('displays that player 1 wins', () => {
+      expect(winner.text()).toEqual('Player 1 wins!');
+    });
+  });
+
+  describe('when player 2 wins', () => {
+    it('displays that player 2 wins', () => {
+      props = { winningPlayer: 'Player 2' };
+      winner = shallow(<Winner {...props} />);
+      expect(winner.text()).toEqual('Player 2 wins!');
+    });
   });
 });
